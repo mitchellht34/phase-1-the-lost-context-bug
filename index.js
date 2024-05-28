4370b59eba40c277e76aad5c945493352a795698
Mitch Toran <mitchellht34@gmail.com>
2024-05-27 22:44:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+  // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
+/* OUTPUT:
+Happy Birthday, Odin One-Eye!
+From Asgard to Nifelheim, you're the best all-father ever.
+
+Love,
+Admiration, respect, and love, Thor
+Your son, Loki
+*/
